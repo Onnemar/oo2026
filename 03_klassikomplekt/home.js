c61e814a70ab0391c5be40992f7b88133ab1e26b
Õnne-Elisabeth Maripu <onnemar@tlu.ee>
2026-02-24 22:55:49 +0000

    kodutöö poolik

--- 03_klassikomplekt/home.js
+++ 03_klassikomplekt/home.js
@@ -17,17 +17,17 @@ var Addable1 = /** @class */ (function () {
     }
     Addable1.prototype.draw = function () {
         //clear previous
-        this.g.clearRect(this.startx, this.y - this.height / 2, this.width, this.height);
+        this.g.clearRect(this.startx + this.width, this.y - this.height / 2, this.width, this.height);
         //================ Draw the rectangle=============
         //x,y,width,height
-        var bodyX = this.startx + this.width / 4;
+        var bodyX = this.startx + this.width;
         var bodyY = this.y - this.height / 2;
-        var bodyW = this.width / 2;
+        var bodyW = this.width;
         var bodyH = this.height;
         this.g.beginPath();
         this.g.rect(bodyX, bodyY, bodyW, bodyH);
         this.g.stroke();
-        this.g.fillText(this.l1 + "Ω", bodyX + 8, this.y + 5);
+        this.g.fillText(this.l1 + "Ω", bodyX + 8, this.y + 8);
     };
     //change the resistors resistanve value nr and redraw it
     Addable1.prototype.setL1 = function (l1) {
@@ -38,6 +38,32 @@ var Addable1 = /** @class */ (function () {
         return this.l1;
     };
     return Addable1;
+}());
+var Plus = /** @class */ (function () {
+    function Plus(g, startx, 
+    //x position where the resistor is going to end
+    endx, 
+    //how far down the screenwe going to draw the resistor. Y increases diagram go
+    y) {
+        this.g = g;
+        this.startx = startx;
+        this.endx = endx;
+        this.y = y;
+        this.xlenght = this.endx - this.startx;
+        this.draw();
+    }
+    ;
+    Plus.prototype.draw = function () {
+        this.g.beginPath();
+        //-line
+        this.g.moveTo(this.startx, this.y);
+        this.g.lineTo(this.endx, this.y);
+        //|line
+        this.g.moveTo(this.startx + 10, this.y - 10);
+        this.g.lineTo(this.startx + 10, this.y + 10);
+        this.g.stroke();
+    };
+    return Plus;
 }());
 var Addable2 = /** @class */ (function () {
     function Addable2(l2, g, 
@@ -58,17 +84,17 @@ var Addable2 = /** @class */ (function () {
     }
     Addable2.prototype.draw = function () {
         //clear previous
-        this.g.clearRect(this.startx, this.y - this.height / 2, this.width, this.height);
+        this.g.clearRect(this.startx + this.width, this.y - this.height / 2, this.width, this.height);
         //================ Draw the rectangle=============
         //x,y,width,height
-        var bodyX = this.startx + this.width / 4;
+        var bodyX = this.startx + this.width;
         var bodyY = this.y - this.height / 2;
-        var bodyW = this.width / 2;
+        var bodyW = this.width;
         var bodyH = this.height;
         this.g.beginPath();
         this.g.rect(bodyX, bodyY, bodyW, bodyH);
         this.g.stroke();
-        this.g.fillText(this.l2 + "Ω", bodyX + 8, this.y + 5);
+        this.g.fillText(this.l2 + "Ω", bodyX + 8, this.y + 8);
     };
     //change the resistors resistanve value nr and redraw it
     Addable2.prototype.setL2 = function (l2) {
@@ -80,15 +106,40 @@ var Addable2 = /** @class */ (function () {
     };
     return Addable2;
 }());
+var Equals = /** @class */ (function () {
+    function Equals(g, startx, 
+    //x position where the resistor is going to end
+    endx, 
+    //how far down the screenwe going to draw the resistor. Y increases diagram go
+    y) {
+        this.g = g;
+        this.startx = startx;
+        this.endx = endx;
+        this.y = y;
+        this.xlen = this.endx - this.startx;
+        this.draw();
+    }
+    Equals.prototype.draw = function () {
+        this.g.beginPath();
+        //-line
+        this.g.moveTo(this.startx, this.y + 3);
+        this.g.lineTo(this.endx, this.y + 3);
+        //2nd -line
+        this.g.moveTo(this.startx, this.y - 3);
+        this.g.lineTo(this.endx, this.y - 3);
+        this.g.stroke();
+    };
+    return Equals;
+}());
 var Total = /** @class */ (function () {
-    function Total(r, g, 
+    function Total(s, g, 
     //x position where the resistor is going to begin
     startx, 
     //x position where the resistor is going to end
     endx, 
     //how far down the screenwe going to draw the resistor. Y increases diagram go
     y) {
-        this.r = r;
+        this.s = s;
         this.g = g;
         this.startx = startx;
         this.endx = endx;
@@ -99,26 +150,25 @@ var Total = /** @class */ (function () {
     }
     Total.prototype.draw = function () {
         //clear previous
-        this.g.clearRect(this.startx, this.y - this.height / 2, this.width, this.height);
+        this.g.clearRect(this.startx + this.width, this.y - this.height / 2, this.width, this.height);
         //================ Draw the rectangle=============
         //x,y,width,height
-        var bodyX = this.startx + this.width / 4;
+        var bodyX = this.startx + this.width;
         var bodyY = this.y - this.height / 2;
-        var bodyW = this.width / 2;
+        var bodyW = this.width;
         var bodyH = this.height;
         this.g.beginPath();
         this.g.rect(bodyX, bodyY, bodyW, bodyH);
         this.g.stroke();
-        var s = this.r + this.r;
-        this.g.fillText(s + "Ω", bodyX + 8, this.y + 5);
+        this.g.fillText(this.s + "Ω", bodyX + 8, this.y + 8);
     };
     //change the resistors resistanve value nr and redraw it
     Total.prototype.setS = function (s) {
-        s = s;
+        this.s = s;
         this.draw();
     };
     Total.prototype.getS = function () {
-        return this.r + this.r;
+        return this.s;
     };
     return Total;
 }());
--- 03_klassikomplekt/home.ts
+++ 03_klassikomplekt/home.ts
@@ -19,17 +19,17 @@ class Addable1{
 
     draw(){
         //clear previous
-        this.g.clearRect(this.startx, this.y-this.height/2, this.width,this.height);
+        this.g.clearRect(this.startx+ this.width, this.y-this.height/2, this.width,this.height);
         //================ Draw the rectangle=============
         //x,y,width,height
-        const bodyX=this.startx+ this.width/4;
+        const bodyX=this.startx+ this.width;
         const bodyY= this.y-this.height/2;
-        const bodyW=this.width/2;
+        const bodyW=this.width;
         const bodyH=this.height;
         this.g.beginPath();
         this.g.rect(bodyX,bodyY,bodyW,bodyH);
         this.g.stroke();
-        this.g.fillText(this.l1+"Ω", bodyX+8, this.y+5);
+        this.g.fillText(this.l1+"Ω", bodyX+8, this.y+8);
 
     }
     //change the resistors resistanve value nr and redraw it
@@ -41,6 +41,44 @@ class Addable1{
         return this.l1;
     }
 }
+
+class Plus{
+    protected xlenght: number;
+;
+
+    constructor(
+        protected g:CanvasRenderingContext2D,
+
+
+       protected startx:number,
+         //x position where the resistor is going to end
+         protected endx: number,
+         //how far down the screenwe going to draw the resistor. Y increases diagram go
+         protected y: number
+    ){
+            this.xlenght=this.endx-this.startx;
+        this.draw();
+    }
+
+    draw(){
+        this.g.beginPath();
+
+        //-line
+        this.g.moveTo(this.startx, this.y);
+        this.g.lineTo(this.endx, this.y);
+        //|line
+        this.g.moveTo(this.startx+10, this.y-10)
+        this.g.lineTo(this.startx+10, this.y+10);
+        this.g.stroke();
+    }
+
+}
+
+
+
+
+
+
 class Addable2{
     protected width:number;
     protected height: number=30;
@@ -62,17 +100,17 @@ class Addable2{
 
     draw(){
         //clear previous
-        this.g.clearRect(this.startx, this.y-this.height/2, this.width,this.height);
+        this.g.clearRect(this.startx+ this.width, this.y-this.height/2, this.width,this.height);
         //================ Draw the rectangle=============
         //x,y,width,height
-        const bodyX=this.startx+ this.width/4;
+        const bodyX=this.startx+ this.width;
         const bodyY= this.y-this.height/2;
-        const bodyW=this.width/2;
+        const bodyW=this.width;
         const bodyH=this.height;
         this.g.beginPath();
         this.g.rect(bodyX,bodyY,bodyW,bodyH);
         this.g.stroke();
-        this.g.fillText(this.l2+"Ω", bodyX+8, this.y+5);
+        this.g.fillText(this.l2+"Ω", bodyX+8, this.y+8);
 
     }
     //change the resistors resistanve value nr and redraw it
@@ -86,12 +124,44 @@ class Addable2{
 }
 
 
+class Equals{
+    protected xlen: number;
+
+    constructor(
+        protected g:CanvasRenderingContext2D,
+
+
+       protected startx:number,
+         //x position where the resistor is going to end
+         protected endx: number,
+         //how far down the screenwe going to draw the resistor. Y increases diagram go
+         protected y: number
+    ){
+            this.xlen=this.endx-this.startx;
+        this.draw();
+    }
+
+    draw(){
+        this.g.beginPath();
+
+        //-line
+        this.g.moveTo(this.startx, this.y+3);
+        this.g.lineTo(this.endx, this.y+3);
+        //2nd -line
+        this.g.moveTo(this.startx, this.y-3)
+        this.g.lineTo(this.endx, this.y-3);
+        this.g.stroke();
+    }
+
+}
+
+ 
 class Total{
     protected width:number;
     protected height: number=30;
 
     constructor(
-        protected r:number,
+        protected s:number,
         protected g:CanvasRenderingContext2D,
         //x position where the resistor is going to begin
         protected startx:number,
@@ -106,26 +176,25 @@ class Total{
 
     draw(){
         //clear previous
-        this.g.clearRect(this.startx, this.y-this.height/2, this.width,this.height);
+        this.g.clearRect(this.startx+ this.width, this.y-this.height/2, this.width,this.height);
         //================ Draw the rectangle=============
         //x,y,width,height
-        const bodyX=this.startx+ this.width/4;
+        const bodyX=this.startx+ this.width;
         const bodyY= this.y-this.height/2;
-        const bodyW=this.width/2;
+        const bodyW=this.width;
         const bodyH=this.height;
         this.g.beginPath();
         this.g.rect(bodyX,bodyY,bodyW,bodyH);
         this.g.stroke();
-        let s=this.r+this.r
-        this.g.fillText(s+"Ω", bodyX+8, this.y+5);
+        this.g.fillText(this.s+"Ω", bodyX+8, this.y+8);
 
     }
     //change the resistors resistanve value nr and redraw it
     setS(s:number){
-        s=s;
+        this.s=s;
         this.draw()
     }
     getS():number{
-        return this.r+this.r;
+        return this.s;
     }
 }

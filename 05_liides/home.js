a82536773585797313fe57a998f15ed06e363936
Õnne-Elisabeth Maripu <onnemar@tlu.ee>
2026-03-15 15:19:31 +0000

    poolik

--- 05_liides/home.js
+++ 05_liides/home.js
@@ -1,37 +1,20 @@
+//fabCalc1
+//density(tihedus)
 var density = /** @class */ (function () {
     function density() {
     }
     density.prototype.calculate = function (reed) {
         return reed / 10;
     };
-    density.prototype.inputUnit = function () {
+    density.prototype.inputUnit1 = function () {
         return "reed nr";
     };
-    density.prototype.outputUnit = function () {
+    density.prototype.outputUnit1 = function () {
         return "warpPerCm";
     };
     return density;
 }());
-//warp quantity (mitu lõime)
-//density*width+(2+2 äärelõime)
-var warpQuan = /** @class */ (function () {
-    function warpQuan() {
-    }
-    warpQuan.prototype.calculate = function (reed, width) {
-        return 2 + (reed / 10) * (width * 1.05) + 2;
-    };
-    warpQuan.prototype.inputUnit1 = function () {
-        return "reed nr";
-    };
-    warpQuan.prototype.inputUnit2 = function () {
-        return "fabric end width";
-    };
-    warpQuan.prototype.outputUnit = function () {
-        return "number of warp threads";
-    };
-    return warpQuan;
-}());
-//warp lenght + shrinkage(1lõime pikkus)
+//full warp length(1lõime pikkus)
 //pikkus*1,2+25+50
 var warpLen = /** @class */ (function () {
     function warpLen() {
@@ -39,14 +22,35 @@ var warpLen = /** @class */ (function () {
     warpLen.prototype.calculate = function (finishedLen) {
         return 25 + (finishedLen * 1.2) + 50;
     };
-    warpLen.prototype.inputUnit = function () {
+    warpLen.prototype.inputUnit1 = function () {
         return "without shrinkage in cm";
     };
-    warpLen.prototype.outputUnit = function () {
+    warpLen.prototype.outputUnit1 = function () {
         return "cm";
     };
     return warpLen;
 }());
+//fabCalc2
+//warp quantity (mitu lõime)
+//tihedus*laius+(2+2 äärelõime)
+var warpQuan = /** @class */ (function () {
+    function warpQuan() {
+    }
+    warpQuan.prototype.calculate = function (reed, width) {
+        return 2 + (reed / 10) * (width * 1.05) + 2;
+    };
+    warpQuan.prototype.inputUnit2 = function () {
+        return "reed nr";
+    };
+    warpQuan.prototype.inputUnit22 = function () {
+        return "fabric end width";
+    };
+    warpQuan.prototype.outputUnit2 = function () {
+        return "number of warp threads";
+    };
+    return warpQuan;
+}());
+//fabCalc3
 //warp material ammount
 var warpMat = /** @class */ (function () {
     function warpMat() {
@@ -54,16 +58,16 @@ var warpMat = /** @class */ (function () {
     warpMat.prototype.calculate = function (reed, width, finishedLen) {
         return (2 + (reed / 10) * (width * 1.05) + 2) + (25 + (finishedLen * 1.2) + 50);
     };
-    warpMat.prototype.inputUnit1 = function () {
+    warpMat.prototype.inputUnit3 = function () {
         return "reed nr";
     };
-    warpMat.prototype.inputUnit2 = function () {
+    warpMat.prototype.inputUnit33 = function () {
         return "fabric end width";
     };
-    warpMat.prototype.inputUnit3 = function () {
+    warpMat.prototype.inputUnit333 = function () {
         return "without shrinkage in cm";
     };
-    warpMat.prototype.outputUnit = function () {
+    warpMat.prototype.outputUnit3 = function () {
         return "cm";
     };
     return warpMat;
@@ -75,16 +79,16 @@ var weftpMat = /** @class */ (function () {
     weftpMat.prototype.calculate = function (reed, finishedWid, finishedLen) {
         return (reed / 10) * (finishedWid * 1.05) + (25 + (finishedLen * 1.2) + 50);
     };
-    weftpMat.prototype.inputUnit1 = function () {
+    weftpMat.prototype.inputUnit3 = function () {
         return "reed nr";
     };
-    weftpMat.prototype.inputUnit2 = function () {
+    weftpMat.prototype.inputUnit33 = function () {
         return "without shrinkage";
     };
-    weftpMat.prototype.inputUnit3 = function () {
+    weftpMat.prototype.inputUnit333 = function () {
         return "without shrinkage in cm";
     };
-    weftpMat.prototype.outputUnit = function () {
+    weftpMat.prototype.outputUnit3 = function () {
         return "cm";
     };
     return weftpMat;
--- 05_liides/home.ts
+++ 05_liides/home.ts
@@ -3,57 +3,37 @@
 interface fabCalc1{
     //x võib olla interfaceist väljas muu nimi. nt cm jne
     calculate(x:number):number;
-    inputUnit():string;
-    outputUnit():string;
+    inputUnit1():string;
+    outputUnit1():string;
 }
 interface fabCalc2{
     //x võib olla interfaceist väljas muu nimi. nt cm jne
     calculate(x1:number,x2:number):number;
-    inputUnit1():string;
     inputUnit2():string;
-    outputUnit1():string;
+    inputUnit22():string;
+    outputUnit2():string;
 }
 interface fabCalc3{
     //x võib olla interfaceist väljas muu nimi. nt cm jne
     calculate(x1:number,x2:number,x3:number):number;
-    inputUnit1():string;
-    inputUnit2():string;
     inputUnit3():string;
-    outputUnit():string;
+    inputUnit33():string;
+    inputUnit333():string;
+    outputUnit3():string;
 }
-
+//fabCalc1
 //density(tihedus)
 class density implements fabCalc1{
     calculate(reed:number):number{
         return reed/10; 
     }
-    inputUnit():string{
+    inputUnit1():string{
         return "reed nr";
     }
-    outputUnit():string{
+    outputUnit1():string{
         return "warpPerCm";
     }  
 }
-
-
-//warp quantity (mitu lõime)
-//tihedus*laius+(2+2 äärelõime)
-class warpQuan implements fabCalc2{
-
-    calculate(reed:number, width:number):number{
-        return 2+(reed/10)*(width*1.05)+2; 
-    }
-     inputUnit1():string{
-        return "reed nr";
-     }
-     inputUnit2():string{
-        return "fabric end width";
-     }
-    outputUnit():string{
-        return "number of warp threads";
-    }
-}
-
 
 //full warp length(1lõime pikkus)
 //pikkus*1,2+25+50
@@ -61,29 +41,52 @@ class warpLen implements fabCalc1{
     calculate(finishedLen:number):number{
         return 25+(finishedLen*1.2)+50; 
     }
-    inputUnit():string{
+    inputUnit1():string{
         return "without shrinkage in cm";
     }
-    outputUnit():string{
+    outputUnit1():string{
         return "cm";
     }
 }
 
+
+//fabCalc2
+//warp quantity (mitu lõime)
+//tihedus*laius+(2+2 äärelõime)
+class warpQuan implements fabCalc2{
+
+    calculate(reed:number, width:number):number{
+        return 2+(reed/10)*(width*1.05)+2; 
+    }
+     inputUnit2():string{
+        return "reed nr";
+     }
+     inputUnit22():string{
+        return "fabric end width";
+     }
+    outputUnit2():string{
+        return "number of warp threads";
+    }
+}
+
+
+
+//fabCalc3
 //warp material ammount
 class warpMat implements fabCalc3{
     calculate(reed:number, width:number,finishedLen:number):number{
         return (2+(reed/10)*(width*1.05)+2)+(25+(finishedLen*1.2)+50)
     }
-    inputUnit1():string{
+    inputUnit3():string{
         return "reed nr";
      }
-    inputUnit2():string{
+    inputUnit33():string{
         return "fabric end width";
      }
-    inputUnit3():string{
+    inputUnit333():string{
         return "without shrinkage in cm";
     }
-    outputUnit():string{
+    outputUnit3():string{
         return "cm";
     }
 }
@@ -92,16 +95,16 @@ class weftpMat implements fabCalc3{
     calculate(reed:number, finishedWid:number, finishedLen:number):number{
         return (reed/10)*(finishedWid*1.05)+(25+(finishedLen*1.2)+50);
     }
-    inputUnit1():string{
+    inputUnit3():string{
         return "reed nr";
     }
-    inputUnit2():string{
+    inputUnit33():string{
         return "without shrinkage";
     }
-    inputUnit3():string{
+    inputUnit333():string{
         return "without shrinkage in cm";
     }
-    outputUnit():string{
+    outputUnit3():string{
         return "cm";
     }
 }
